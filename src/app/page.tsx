4ef304eb74eefcf3ad304af0bd6416fd64002fec
Your Name <you@example.com>
2024-02-16 21:31:36 +0700

    docs: use swr all api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
 'use client'
 
-import Link from 'next/link'
+import AppTable from '@/components/app.table'
 import styles1 from '@/styles/app.module.css'
 import styles2 from '@/styles/test.module.css'
-import AppTable from '@/components/app.table'
-import { useEffect } from 'react'
+import Link from 'next/link'
+import useSWR from "swr"
 
 export default function Home() {
-  const fetchBlogs = async () => {
-    const res = await fetch("http://localhost:8000/blogs")
-    const data = await res.json()
-    console.log('data: ', data)  
-  }
+  const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-  useEffect(() => {
-    fetchBlogs()
-  }, [])
+  // Caching Data
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
+    }
+  )
+
+  console.log('data: ', data)
 
   return (
     <div>
+      <div>Data: {data?.length}</div>
       <ul>
         <li style={{ marginBottom: '20px' }} className={styles1['facebook']}>
           <Link href='/facebook'><div className={styles2['facebook']}>Facebook</div></Link>
