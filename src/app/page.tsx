459e98a9ac6e1ef235c72eb74d36a6796fb9d384
Your Name <you@example.com>
2024-02-16 20:44:59 +0700

    docs: call api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,19 @@ import Link from 'next/link'
 import styles1 from '@/styles/app.module.css'
 import styles2 from '@/styles/test.module.css'
 import AppTable from '@/components/app.table'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const fetchBlogs = async () => {
+    const res = await fetch("http://localhost:8000/blogs")
+    const data = await res.json()
+    console.log('data: ', data)  
+  }
+
+  useEffect(() => {
+    fetchBlogs()
+  }, [])
+
   return (
     <div>
       <ul>
