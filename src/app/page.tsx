73de949581d2d84811a871aa5701d55b5dca2d4d
Your Name <you@example.com>
2024-02-16 23:12:52 +0700

    feat: design modal add new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import AppTable from '@/components/app.table'
+import CreateModal from '@/components/create.modal'
 import styles1 from '@/styles/app.module.css'
 import styles2 from '@/styles/test.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
 import useSWR from "swr"
 
 export default function Home() {
+  const [showCreateModal, setShowCreateModal] = useState<boolean>(false)
+
   const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
   // Caching Data
@@ -35,7 +40,21 @@ export default function Home() {
           <a href='/tiktok'>Tiktok</a>
         </li>
       </ul>
+      
+      <div 
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}
+        className='mb-3'
+      >
+        <h3>Table Blogs</h3>
+        <Button onClick={() => setShowCreateModal(true)}>Add New</Button>
+      </div>
+
       <AppTable blogs={data}/>
+
+      <CreateModal showCreateModal={showCreateModal} setShowCreateModal={setShowCreateModal}/>
     </div>
   )
 }
---
+++ src/components/create.modal.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import { useState } from 'react';
+
+interface IProps {
+  showCreateModal: boolean;
+  setShowCreateModal: (value: boolean) => void;
+}
+
+function CreateModal(props: IProps) {
+  const [title, setTitle] = useState<string>('')
+  const [author, setAuthor] = useState<string>('')
+  const [content, setContent] = useState<string>('')
+
+  const { showCreateModal, setShowCreateModal } = props
+
+  const handleSubmit = () => {
+    console.log('data form: ', { title, author, content })
+  }
+
+  const handleClose = () => {
+    setTitle('')
+    setAuthor('')
+    setContent('')
+    setShowCreateModal(false)
+  }
+
+  return (
+    <>
+      <Modal
+        show={showCreateModal}
+        onHide={() => handleClose()}
+        backdrop="static"
+        keyboard={false}
+        size='lg'
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add New A Blog</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Author</Form.Label>
+              <Form.Control type="text" value={author} onChange={(e) => setAuthor(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Content</Form.Label>
+              <Form.Control as="textarea" rows={3} value={content} onChange={(e) => setContent(e.target.value)}/>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => handleClose()}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handleSubmit()}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default CreateModal
