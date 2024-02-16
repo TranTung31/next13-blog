368e8c1bfbe72201d1e8a7bf9ee081238ac98f93
Your Name <you@example.com>
2024-02-16 14:10:16 +0700

    routing_p2

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,6 +1,21 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
 function Facebook() {
+  const router = useRouter()
+
+  const handleBtn = () => {
+    router.push('/')
+  }
+
   return (
-    <div>Facebook Page</div>
+    <div>
+      Facebook Page
+      <div style={{ marginTop: '20px' }}>
+        <button onClick={() => handleBtn()}>Back</button>
+      </div>
+    </div>
   )
 }
 
