d3d6fc2cfcde53b2bfb7a37f6b9449f9fef49654
Your Name <you@example.com>
2024-02-18 11:18:51 +0700

    feat: design page blog detail

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,6 +1,22 @@
+'use client'
+
+import Link from 'next/link'
+import Card from 'react-bootstrap/Card'
+
 function BlogDetail({ params }: { params: { id: string } }) {
   return (
-    <div>Blog Detail {params.id}</div>
+    <div className='my-3'>
+      <Link href='/blogs' className='btn btn-primary mb-3'>Go back</Link>
+      <Card className="text-center">
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
+    </div>
   )
 }
 
